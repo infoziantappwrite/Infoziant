b4355530907a9d089da184c2aa92aba0a0dc24c9
Murali v <v.murali1818@gmail.com>
2025-04-09 20:35:17 +0530

    pages done'

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,17 +2,23 @@ import React from 'react'
 import Banner from './Banner/Banner';
 import Ourservices from './Ourservices/Ourservices';
 import AboutUsSection from './AboutSection/AboutUsSection';
+import ServicesSection from './Services1/ServicesSection';
+import InnovativeTech from './Services1/InnovativeTech';
 
 const Home = () => {
   return (
     <>
         <Banner />
         <AboutUsSection />
+      
         {/* <CounterSection /> */}
         {/* <Testimonials /> */}
         {/* <BlogSection /> */}
         {/* <ContactSection /> */}
         <Ourservices />
+        <ServicesSection />
+        <InnovativeTech />
+        {/* <Footer /> */}
       
     </>
   )
