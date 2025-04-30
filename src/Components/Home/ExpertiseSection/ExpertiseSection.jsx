0f6997091c5d6a3ab0d26c701a0850fbff38335f
Sundar Karthik S <sundar1007karthik@gmail.com>
2025-04-30 23:20:57 +0530

    Enhance ExpertiseSection with additional achievements and tools; update icons for improved visual representation

--- src/Components/Home/ExpertiseSection/ExpertiseSection.jsx
+++ src/Components/Home/ExpertiseSection/ExpertiseSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ChevronLeft, ChevronRight, CheckCircle, Award, Trophy } from "lucide-react";
+import { ChevronLeft, ChevronRight, CheckCircle, CirclePlay, Star, Award, FileBadge2, Sparkles, Trophy } from "lucide-react";
 import cybersecurity from "./cybersecurity.png"
 import webandapp from "./webandapp.png"
 import aiml from "./aiml.png";
@@ -18,10 +18,14 @@ const slides = [
             "Generate Audit Reports"
         ],
         achievements: [
-            "Secured 200+ systems for global clients",
-            "Saved $12.7 M for an Insurance provider"
+            "$12.7 M Saved for an Insurance provider",
+            "81+ Rewards and Recognition"
         ],
         tools: [
+            "Appreciated by US Environmental Protection Agency",
+            "Appreciated by Bureau of Indian Affairs"
+        ],
+        one: [
             "Appreciated by US Environmental Protection Agency",
             "Appreciated by Bureau of Indian Affairs"
         ]
@@ -36,9 +40,14 @@ const slides = [
             "Secure CMS & e-commerce portals"
         ],
         achievements: [
-            "150+ web & mobile projects delivered",
-            "200+ custom API integrations"
+            "40+ Happy Clients",
+            "70+ Web & Mobile Apps Delivered",
+        ], 
+        tools: [
+            "Highly skilled Developers",
+            "Zero Defect Policy"
         ]
+
     },
     {
         title: "AI & Machine Learning",
@@ -52,6 +61,10 @@ const slides = [
         achievements: [
             "50+ AI solutions deployed globally",
             "100+ LLMs tested & validated"
+        ],
+        tools: [
+            "Expertise beyond measure",
+            "Transformative AI Solutions"
         ]
     },
     {
@@ -64,9 +77,13 @@ const slides = [
             "Specialized in e-learning & game testing"
         ],
         achievements: [
-            "1M+ test cases automated",
-            "1,000+ web & 500+ mobile apps tested"
-        ]
+            "400+ Successful Projects",
+            "650+ Apps Automated"
+        ],
+        tools: [
+            "Zero Flaky Tests",
+            "Accelerate Time to Market"
+        ]   
     },
     {
         title: "CodeChef Training",
@@ -78,8 +95,12 @@ const slides = [
             "Certificates & Leaderboards"
         ],
         achievements: [
-            "23,000+ students trained",
-            "Trusted by 20+ Institutions"
+            "10M+ Users",
+            "150+ Countries"
+        ],
+        tools: [
+            "In-demand programming languages",
+            "Gain practical, job-ready skills."
         ]
     },
     {
@@ -92,14 +113,21 @@ const slides = [
             "Industry-Academic Tie-ups"
         ],
         achievements: [
-            "30+ partner colleges",
+            "37+ Institutions",
             "7,510+ students placed"
+        ],
+        tools: [
+            "companies offering CTCs up to ₹1.02 Cr",
+            "18000+ Happy Students"
         ]
     }
 ];
 
+
+
 const ExpertiseSection = () => {
     const [selected, setSelected] = useState(0);
+    const isOne = 0;
 
     const prev = () => setSelected((prev) => (prev > 0 ? prev - 1 : slides.length - 1));
     const next = () => setSelected((prev) => (prev + 1) % slides.length);
@@ -160,60 +188,65 @@ const ExpertiseSection = () => {
                                             className="h-[200px] lg:h-[300px] w-auto object-cover rounded-lg"
                                         />
                                     </div>
-
-                                    {/* Text Content Section */}
                                     <div className="w-full md:w-1/2 p-4 md:p-5 overflow-y-auto space-y-4 max-h-[500px]">
-                                        <h3 className="text-xl md:text-2xl font-bold text-blue-700">{slides[selected].title}</h3>
+                                                                            <h3 className="text-xl md:text-2xl font-bold text-blue-700">{slides[selected].title}</h3>
 
-                                        <div>
-                                            <ul className="space-y-2 pl-2 text-gray-700">
-                                                {slides[selected].keyHighlights.map((point, i) => (
-                                                    <li key={i} className="flex items-start gap-2">
-                                                        <CheckCircle className="text-blue-500 mt-1" size={18} />
-                                                        <span>{point}</span>
-                                                    </li>
-                                                ))}
-                                            </ul>
-                                        </div>
-                                        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center">
-                                            {slides[selected].achievements.map((point, i) => (
-                                                <div
-                                                    key={i}
-                                                    className="flex flex-col items-center text-center bg-gradient-to-r from-cyan-500 via-teal-500 to-blue-500 text-white px-4 py-5 rounded-xl shadow-md text-sm font-semibold transition-transform duration-300 hover:scale-105"
-                                                >
-                                                    <Trophy size={28} className="text-white mb-2 drop-shadow-sm" />
-                                                    <span>{point}</span>
-                                                </div>
-                                            ))}
-                                        </div>
-
-
-
-                                    </div>
-
-                                </div>
-                                <div className="text-center pb-4">
-                                    {slides[selected].tools && (
-                                        <ul className="flex flex-wrap justify-center gap-4 mt-4">
-                                            {slides[selected].tools.map((tool, i) => (
-                                                <li
-                                                    key={i}
-                                                    className="flex items-center gap-2 px-4 py-2 rounded-full bg-gradient-to-r from-yellow-400 via-orange-400 to-red-500 text-white font-semibold text-md  shadow-lg hover:scale-105 transition-transform duration-300"
-                                                >
-                                                    <Award size={24} className="text-white drop-shadow-sm" />
-                                                    {tool}
-                                                </li>
-                                            ))}
-                                        </ul>
-                                    )}
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
+                                                                            <div>
+                                                                                <ul className="space-y-2 pl-2 text-gray-700">
+                                                                                    {slides[selected].keyHighlights.map((point, i) => (
+                                                                                        <li key={i} className="flex items-start gap-2">
+                                                                                            <CheckCircle className="text-blue-500 mt-1" size={18} />
+                                                                                            <span>{point}</span>
+                                                                                        </li>
+                                                                                    ))}
+                                                                                </ul>
+                                                                            </div>
+                                                                            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 justify-center">
+                                                                                {slides[selected].achievements.map((point, i) => (
+                                                                                    <div
+                                                                                        key={i}
+                                                                                        className="flex flex-col items-center text-center bg-gradient-to-r from-cyan-500 via-teal-500 to-blue-500 text-white px-4 py-5 rounded-xl shadow-md text-sm font-semibold transition-transform duration-300 hover:scale-105"
+                                                                                    >
+                                                                                        <Trophy size={28} className="text-white mb-2 drop-shadow-sm" />
+                                                                                        <span>{point}</span>
+                                                                                    </div>
+                                                                                ))}
+                                                                            </div>
 
 
-                    {/* Arrow Navigation */}
+
+                                                                        </div>
+
+                                                                    </div>
+
+                                                                    <div className="text-center pb-4">
+                                                                        {slides[selected].tools && (
+                                                                            <ul className="flex flex-wrap justify-center gap-4 mt-4">
+                                                                                {slides[selected].tools.map((tool, i) => (
+                                                                                    <li
+                                                                                        key={i}
+                                                                                        className="flex items-center gap-2 px-4 py-2 rounded-full bg-gradient-to-r from-yellow-400 via-orange-400 to-red-500 text-white font-semibold text-md  shadow-lg hover:scale-105 transition-transform duration-300"
+                                                                                    >
+                                                                                        {selected === 0 ? <Award size={24} className="text-white drop-shadow-sm" /> : <Sparkles size={24} className="text-white drop-shadow-sm" />}
+
+                                                                                        {tool}
+                                                                                    </li>
+                                                                                ))}
+                                                                            </ul>
+                                                                        )}
+                                                                    </div>
+
+                                                                   
+                                                                </div>
+
+                                                            </div>
+                                                        </div>
+
+
+
+                                                                        
+
+                                                        {/* Arrow Navigation */}
                     <div className="flex items-center gap-4 mt-4">
                         <button
                             onClick={prev}
